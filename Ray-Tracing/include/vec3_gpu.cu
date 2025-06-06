9a5936e3328825fc59bacfe5f13e864d5dee1354
Refiloe Mopeloa <72868391+refiloemopeloa@users.noreply.github.com>
2025-06-06 03:40:02 +0200

    Added more functionality for vec3 class.
    - Added running total operator overloading.
    - Added method for making current vector a unit vector.

--- Ray-Tracing/include/vec3_gpu.cu
+++ Ray-Tracing/include/vec3_gpu.cu
@@ -58,9 +58,10 @@ __host__ __device__ inline vec3 operator*(const float t, const vec3 &v1) {
 __host__ __device__ inline vec3 operator/(const vec3 &v1, const float t) {
     return vec3(v1.e[0] / t, v1.e[1] / t, v1.e[2] / t);
 }
-__host__ __device__ inline vec3 operator/(const float t, const vec3 &v1) {
-    return vec3(v1.e[0] / t, v1.e[1] / t, v1.e[2] / t);
-}
+
+// __host__ __device__ inline vec3 operator/(const float t, const vec3 &v1) {
+//     return vec3(v1.e[0] / t, v1.e[1] / t, v1.e[2] / t);
+// }
 
 __host__ __device__ inline float dot(const vec3 &v1, const vec3 &v2) {
     return v1.e[0] * v2.e[0] + v1.e[1] * v2.e[1] + v1.e[2] * v2.e[2];
@@ -70,6 +71,55 @@ __host__ __device__ inline vec3 cross(const vec3 &v1, const vec3 &v2) {
     v1.e[2] * v2.e[0] - v1.e[0] * v2.e[2],
     v1.e[0] * v2.e[1] - v1.e[1] * v2.e[0]);
 }
+
+__host__ __device__ inline vec3& vec3::operator+=(const vec3 &v2) {
+    e[0] += v2.e[0];
+    e[1] += v2.e[1];
+    e[2] += v2.e[2];
+    return *this;
+}
+
+__host__ __device__ inline vec3& vec3::operator/=(const float t) {
+    e[0] /= t;
+    e[1] /= t;
+    e[2] /= t;
+    return *this;
+}
+
+__host__ __device__ inline vec3& vec3::operator*=(const vec3 &v){
+    e[0]  *= v.e[0];
+    e[1]  *= v.e[1];
+    e[2]  *= v.e[2];
+    return *this;
+}
+
+__host__ __device__ inline vec3& vec3::operator/=(const vec3 &v){
+    e[0]  /= v.e[0];
+    e[1]  /= v.e[1];
+    e[2]  /= v.e[2];
+    return *this;
+}
+
+__host__ __device__ inline vec3& vec3::operator-=(const vec3& v) {
+    e[0]  -= v.e[0];
+    e[1]  -= v.e[1];
+    e[2]  -= v.e[2];
+    return *this;
+}
+
+__host__ __device__ inline vec3& vec3::operator*=(const float t) {
+    e[0]  *= t;
+    e[1]  *= t;
+    e[2]  *= t;
+    return *this;
+}
+
+
 __host__ __device__ inline vec3 unit_vector(vec3 v) {
     return v / v.length();
 };
+
+__host__ __device__ inline void vec3::make_unit_vector() {
+    float k = 1.0 / sqrt(e[0]*e[0] + e[1]*e[1] + e[2]*e[2]);
+    e[0] *= k; e[1] *= k; e[2] *= k;
+}
